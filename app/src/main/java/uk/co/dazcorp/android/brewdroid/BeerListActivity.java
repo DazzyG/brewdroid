801776a8a06f46ef71513ec0bf33baf8731e348d
Daryl Gent <daryl.gent@monitisecreate.com>
2016-12-21 18:29:16 +0000

    auto open first fragment - add firebase notifications

--- app/src/main/java/uk/co/dazcorp/android/brewdroid/BeerListActivity.java
+++ app/src/main/java/uk/co/dazcorp/android/brewdroid/BeerListActivity.java
@@ -1,18 +1,43 @@
 package uk.co.dazcorp.android.brewdroid;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class BeerListActivity extends BaseActivity {
+
+    private BeerListFragment mBeerfragment;
+    private Fragment mCurrentFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer_list);
-        BeerListFragment fragment = new BeerListFragment();
 
-        FragmentManager fm = getSupportFragmentManager();
-        fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        mBeerfragment = new BeerListFragment();
+        final FragmentManager fm = getSupportFragmentManager();
+
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation_view);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.all_beers:
+                        if (mCurrentFragment == null || mCurrentFragment != mBeerfragment) {
+                            mCurrentFragment = mBeerfragment;
+                            fm.beginTransaction().add(R.id.fragment_container, mBeerfragment).commit();
+                        }
+                        return true;
+                    case R.id.favourites:
+                        break;
+                }
+                return false;
+            }
+        });
+
+        findViewById(navigationView.getMenu().getItem(0).getItemId()).performClick();
     }
 }
